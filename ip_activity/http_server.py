165e707d6371912806cb63e8d48bb46d644741a2
Katerina Pilatova <xpilat05@stud.fit.vutbr.cz>
2016-04-19 22:27:11 +0200

    http_server.py: added basic functionality, compatibility in progress
    - create_request_handler validates config file, calculates needed sizes
    -                        modifies HTTPRequestHandler with attributes etc
    - binary_read: reads data from bitmap file, transposes for visualisation

--- ip_activity/http_server.py
+++ ip_activity/http_server.py
@@ -42,175 +42,199 @@
 # if advised of the possibility of such damage.
 #
 
-import argparse
-import BaseHTTPServer
-import cgi
-import logging
-import os
-import sys
-
 from __future__ import print_function, division, with_statement
 
-class ip_activity_RequestHandler(BaseHTTPServer.BaseHTTPRequestHandler):
-   def __init__(self, args):
-      ''' Saves options for later use '''
-      self.arguments = args
+import argparse
+from bitarray import bitarray
+import cgi
+import ipaddress
+import logging
+import math
+import os
+import yaml # pyyaml
+import sys
 
-   def do_GET(self):
-      ''' Handle a HTTP GET request. '''
-      logging.debug('GET %s' % (self.path))
+# Python 2.x BaseHTTPServer is in http.server in Python 3.x
+if sys.version_info[0] == 2:
+   from BaseHTTPServer import BaseHTTPRequestHandler, HTTPServer
+else:
+   from http.server import BaseHTTPRequestHandler, HTTPServer
 
-      # Parse URL arguments
-      index = self.path.find('?')
+MAX_WINDOW = 1000
+MAX_VECTOR_SIZE = 1000
 
-      if index >= 0:
-         get_path = self.path[:index]
-         query = cgi.parse_qs(self.path[index+1:])
-      else:
-         get_path = self.path
+SRC = 0
+DST = 1
+SRC_DST = 2
 
-      # Print out logging information about the path and args.
-      if 'content-type' in self.headers:
-         ctype, _ = cgi.parse_header(self.headers['content-type'])
-         logging.debug('TYPE %s' % (ctype))
 
-      logging.debug('PATH %s' % (rpath))
-      logging.debug('ARGS %d' % (len(args)))
-      if len(args):
-         i = 0
-         for key in sorted(args):
-            logging.debug('ARG[%d] %s=%s' % (i, key, args[key]))
-            i += 1
+def create_request_handler(args):
+   ''' Creates class for handling requests, adds needed variables '''
 
-      # Check to see whether the file is stored locally,
-      # if it is, display it.
+   # Read configuration file
+   try:
+      with open(args['config'], 'r') as fd:
+         config_file = yaml.load(fd.read())
+   except IOError:
+      print('File ' + args['config'] + ' could not be opened.')
+      sys.exit(1)
 
-       # Get the file path.
-       new_path = ip_activity_RequestHandler.arguments['dir'] + get_path
+   # Check if node with filename exists:
+   if args['filename'] not in config_file:
+      print('Bitmap files not found.', file=sys.stderr)
+      sys.exit(1) 
 
-       dirpath = None
-       logging.debug('FILE %s' % (path))
+   # Check if node contains required keys
+   if (not(set(['time', 'addresses'])<= set(config_file[args['filename']].keys())) or
+      not(set(['first', 'last', 'granularity']) <= set(config_file[args['filename']]['addresses'].keys())) or
+      not(set(['granularity', 'intervals']) <= set(config_file[args['filename']]['time'].keys()))):
+      print('Configuration file structure is invalid.', file=sys.stderr)
 
-       # If it is a directory look for index.html
-       # or process it directly if there are 3
-       # trailing slashed.
-       if rpath[-3:] == '///':
-          dirpath = path
-       elif os.path.exists(path) and os.path.isdir(path):
-          dirpath = path  # the directory portion
-          index_files = ['/index.html', '/index.htm', ]
-          for index_file in index_files:
-             tmppath = path + index_file
-             if os.path.exists(tmppath):
-                path = tmppath
-                break
+   # Get IPs
+   first_ipaddr = None
+   last_ipaddr = None
+   if sys.version_info[0] == 2:
+      first_ipaddr = ipaddress.ip_address(unicode(config_file[args['filename']]['addresses']['first'],
+                                                  "utf-8"))
+      last_ipaddr = ipaddress.ip_address(unicode(config_file[args['filename']]['addresses']['last'],
+                                                 "utf-8"))
 
-       # Allow the user to type "///" at the end to see the
-       # directory listing.
-       if os.path.exists(path) and os.path.isfile(path):
-          # This is valid file, send it as the response
-          # after determining whether it is a type that
-          # the server recognizes.
-          _, ext = os.path.splitext(path)
-          ext = ext.lower()
-          content_type = {
-             '.css': 'text/css',
-             '.gif': 'image/gif',
-             '.htm': 'text/html',
-             '.html': 'text/html',
-             '.jpeg': 'image/jpeg',
-             '.jpg': 'image/jpg',
-             '.js': 'text/javascript',
-             '.png': 'image/png',
-             '.text': 'text/plain',
-             '.txt': 'text/plain',
-          }
+   else:
+      first_ipaddr = ipaddress.ip_address(config_file[args['filename']]['addresses']['first'])
 
-          # If it is a known extension, set the correct
-          # content type in the response.
-          if ext in content_type:
-             self.send_response(200)  # OK
-             self.send_header('Content-type', content_type[ext])
-             self.end_headers()
+      last_ipaddr = ipaddress.ip_address(config_file[args['filename']]['addresses']['last'])
 
-             with open(path) as ifp:
-                self.wfile.write(ifp.read())
-          else:
-             # Unknown file type or a directory.
-             # Treat it as plain text.
-             self.send_response(200)  # OK
-             self.send_header('Content-type', 'text/plain')
-             self.end_headers()
+   ip_granularity = int(config_file[args['filename']]['addresses']['granularity'])
 
-             with open(path) as ifp:
-                self.wfile.write(ifp.read())
-       else:
-          if dirpath is None or self.m_opts.no_dirlist == True:
-             # Invalid file path, respond with a server access error
-             self.send_response(500)  # generic server error for now
-             self.send_header('Content-type', 'text/html')
-             self.end_headers()
+   # Adjust IPs to granularity
+   ip_size = first_ipaddr.max_prefixlen
+   shifted_first = ipaddress.ip_address((int(first_ipaddr) >> (ip_size - ip_granularity)))
+   shifted_last = ipaddress.ip_address((int(last_ipaddr) >> (ip_size - ip_granularity)))
 
-             self.wfile.write('<html>')
-             self.wfile.write('  <head>')
-             self.wfile.write('    <title>Server Access Error</title>')
-             self.wfile.write('  </head>')
-             self.wfile.write('  <body>')
-             self.wfile.write('    <p>Server access error.</p>')
-             self.wfile.write('    <p>%r</p>' % (repr(self.path)))
-             self.wfile.write('    <p><a href="%s">Back</a></p>' % (rpath))
-             self.wfile.write('  </body>')
-             self.wfile.write('</html>')
-          else:
-             # List the directory contents. Allow simple navigation.
-             logging.debug('DIR %s' % (dirpath))
+   # Get vector size
+   vector_size = int(shifted_first) - int(shifted_last)
 
-             self.send_response(200)  # OK
-             self.send_header('Content-type', 'text/html')
-             self.end_headers()
+   # Get time window
+   window = int(config_file[args['filename']]['time']['intervals'])
 
-             self.wfile.write('<html>')
-             self.wfile.write('  <head>')
-             self.wfile.write('    <title>%s</title>' % (dirpath))
-             self.wfile.write('  </head>')
-             self.wfile.write('  <body>')
-             self.wfile.write('    <a href="%s">Home</a><br>' % ('/'));
+   # Create empty bitmaps
+   bitmap_src = {}
+   for i in range(vector_size):
+      bitmap_s[i] = bitarray()
 
-             # Make the directory path navigable.
-             dirstr = ''
-             href = None
-             for seg in rpath.split('/'):
-                if href is None:
-                   href = seg
-                else:
-                   href = href + '/' + seg
-                   dirstr += '/'
-                dirstr += '<a href="%s">%s</a>' % (href, seg)
-             self.wfile.write('    <p>Directory: %s</p>' % (dirstr))
+   bitmap_dst = {}
+   for i in range(vector_size):
+      bitmap_dst[i] = bitarray()
 
-             # Write out the simple directory list (name and size).
-             self.wfile.write('    <table border="0">')
-             self.wfile.write('      <tbody>')
-             fnames = ['..']
-             fnames.extend(sorted(os.listdir(dirpath), key=str.lower))
-             for fname in fnames:
-                self.wfile.write('        <tr>')
-                self.wfile.write('          <td align="left">')
-                path = rpath + '/' + fname
-                fpath = os.path.join(dirpath, fname)
-                if os.path.isdir(path):
-                   self.wfile.write('            <a href="%s">%s/</a>' % (path, fname))
-                else:
-                   self.wfile.write('            <a href="%s">%s</a>' % (path, fname))
-                self.wfile.write('          <td>&nbsp;&nbsp;</td>')
-                self.wfile.write('          </td>')
-                self.wfile.write('          <td align="right">%d</td>' % (os.path.getsize(fpath)))
-                self.wfile.write('        </tr>')
-             self.wfile.write('      </tbody>')
-             self.wfile.write('    </table>')
-             self.wfile.write('  </body>')
-             self.wfile.write('</html>')
+   bitmap_srcdst = {}
+   for i in range(vector_size):
+      bitmap_srcdst[i] = bitarray()
 
+   # Create additional attributes and methods in handler
+   class My_RequestHandler(BaseHTTPRequestHandler):
+
+      # Input information
+      arguments = args
+
+      # IPs + subnet size
+      first_ip = first_ipaddr
+      last_ip = last_ipaddr
+      granularity = ip_granularity
+
+      # Size of bit vectors (rows) in file [b]
+      bit_vector_size = vector_size
+      byte_vector_size = int(math.ceil(bit_vector_size / 8))
+
+      # Time unit and window size
+      time_interval = int(config_file[args['filename']]['time']['granularity'])
+      time_window = window
+      
+      # Bitmaps
+      bitmap_s = bitmap_src
+      bitmap_d = bitmap_dst
+      bitmap_sd = bitmap_srcdst
+
+      def binary_read(self, filename):
+         ''' Returns 2D bitarray of updated, transposed bitmap '''
+
+         filesize = os.path.getsize(filename)
+         rows = filesize / byte_vector_size
+
+         # Temporary bitmap for rows vectors
+         tmp_bitmap = {}
+         for i in range(rows):
+            tmp_bitmap[i] = bitarray()
+
+
+         # Reads file row by row (by intervals), fill tmp_bitmap
+         # 1 row == the whole address space in 1 time interval
+         try:
+            with open(filename, 'rb') as fd:
+               for r in range(rows):
+                     byte_vector = fd.read(byte_vector_size)
+                     tmp_bitmap[r].frombytes(byte_vector)
+
+         except IOError:
+            print('File ' + args['config'] + ' could not be opened.')
+            sys.exit(1)
+
+         # Remove padding from the end of the vector if needed
+         trim_size = 8-(bit_vector_size % 8)
+         if trim_size < 8:
+            for r in range(rows):
+               tmp_bitmap[r] = tmp_bitmap[r][:-trim]
+
+         # Transpose bitmap
+         # 1 row == 1 IP in all intervals
+         transp_bitmap = {}
+         for i in range(bit_vector_size):
+            transp_bitmap[i] = bitarray()
+
+         # Go through intervals in tmp_bitmap, append to transposed
+         for r in range(rows):
+            for b in range(bit_vector_size):
+               transp_bitmap[b].append(tmp_bitmap[r][b])
+
+         return transp_bitmap
+   
+      def do_GET(self):
+         ''' Handle a HTTP GET request. '''
+
+         print('GET ' + self.path)
+   
+         # Parse URL arguments
+         index = self.path.find('?')
+   
+         if index >= 0:
+            get_path = self.path[:index]
+            query = cgi.parse_qs(self.path[index+1:])
+         else:
+            get_path = self.path
+   
+         # Print out logging information about the path and args.
+         if 'content-type' in self.headers:
+            ctype, _ = cgi.parse_header(self.headers['content-type'])
+            print('TYPE ' + ctype)
+   
+         print('PATH ' + get_path)
+         print('ARGS ' + len(query))
+         if len(args):
+            i = 0
+            for key in sorted(args):
+               print ('ARG[' + i + ']: ' + key + ' = ' + args[key])
+               i += 1
+
+         self.send_response(200)  # OK
+         self.send_header('Content-type', 'text/html')
+         self.end_headers()
+      
+         self.wfile.write('<html>')
+         self.wfile.write('<body>')
+         self.wfile.write('Sasa je trdlo.')
+         self.wfile.write('</body>')
+         self.wfile.write('</html>')
+
+   return My_RequestHandler
 
 def main():
    ''' Main function for the lifecycle of the server. '''
@@ -223,12 +247,14 @@ def main():
    parser.add_argument('-a', '--all', action='store_true', help='Fetch all types of bitmaps.')
    parser.add_argument('-p', '--port', type=int, default=8080,
                        help='Server port, (8080 by default)')
-   parser.add_argument('-h', '--hostname', type=str, default='localhost',
+   parser.add_argument('-H', '--hostname', type=str, default='localhost',
                        help='Server hostname (localhost by default).')
-   parser.add_argument('-d', '--dir', type=str, default='./', required=True,
+   parser.add_argument('-d', '--dir', type=str, default='./',
                        help='Path to directory with web client files (HTML, CSS, JS) (current directory by default).')
    parser.add_argument('-c', '--config', type=str, default='./', required=True,
                        help='Path to configuration file (current directory by default).')
+   parser.add_argument('-f', '--filename', type=str, default='bitmap', required=False,
+                       help='Filename of bitmap storage files (bitmap by default).')
    args = vars(parser.parse_args())
 
    # Check their validity
@@ -245,8 +271,9 @@ def main():
       sys.exit(1)
 
    # Create the server
-   server = BaseHTTPServer.HTTPServer((args['hostname'], args['port']),
-                                      ip_activity_RequestHandler)
+   ip_activity_RequestHandler = create_request_handler(args)
+
+   server = HTTPServer((args['hostname'], args['port']), ip_activity_RequestHandler)
 
    # Serve forever
    try:
